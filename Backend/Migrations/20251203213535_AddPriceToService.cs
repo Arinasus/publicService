e5ac63b533cb7c4d4d5dca393f971e2d63e8011b
Arinana <allyut12zk@gmail.com>
2025-12-04 01:08:44 +0300

    Добавление функционала администрутору
    Для страницы Услуги добавлены операции изменения, удаления, добавления. Для страницы Счета добавлена фильтрация.

---
+++ Backend/Migrations/20251203213535_AddPriceToService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                schema: "public",
+                table: "Services",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                schema: "public",
+                table: "Services");
+        }
+    }
+}
